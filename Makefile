91056b64b31e079faf5c970e11b5ba0b7dab3bdb
Saurabh <saurabhdeo27@gmail.com>
2017-05-27 19:02:36 -0700

    Working SUPER and GROUP summaries

---
+++ Makefile
@@ -0,0 +1,21 @@
+# NAMES: Priscilla Cheng and Saurabh Deo
+# EMAILS: priscillaccheng@gmail.com and saurabhdeo27@gmail.com
+# IDS: 404159386 and 404616605
+
+SOURCES = lab3a.c
+EXECNAME = lab3a
+CC = gcc
+DEBUG = -g
+LFLAGS = -Wall -Werror $(DEBUG) 
+TARCONTENTS = lab3a.c #TODO: Add to this
+TARNAME = lab3a_404159386_404616605.tar.gz  #TODO: Check this!
+
+
+lab3a:
+	$(CC) $(LFLAGS) $(SOURCES) -o $(EXECNAME)
+
+clean:
+	@- rm -rf lab3a $(TARNAME)
+
+dist:
+	tar -czvf $(TARNAME) $(TARCONTENTS) 
--- lab3a.c
+++ lab3a.c
@@ -13,10 +13,6 @@
 //some silly notes about my naming convention feel free to change to what u feel is best
 //using underscores for sizes/values and camelCase for structures
 
-char reportBuf[1024];
-struct ext2_super_block sb;
-struct ext2_group_desc groupDesc;
-
 /* SUPER BLOCK VARIABLES */
 
 int i;
@@ -32,8 +28,13 @@ int free_blocks, free_inodes;
 int block_bitmap_blk_num, inode_bitmap_blk_num, first_inode_blk_num;
 int num_group;
 
+char reportBuf[1024];
+struct ext2_super_block sb;
+struct ext2_group_desc groupDesc;
+
 
 void superblockSummary() {
+	pread(ext2_fd, &sb, sizeof(sb), 1024);	
 	block_size = EXT2_MIN_BLOCK_SIZE << sb.s_log_block_size; 
 	inode_size = sb.s_inode_size;
 	num_blocks = sb.s_blocks_count;
@@ -42,30 +43,29 @@ void superblockSummary() {
 	inodes_per_group = sb.s_inodes_per_group;
 	first_nonres_inode = sb.s_first_ino;
 	
-	pread(ext2_fd, &sb, sizeof(sb), 1024);
 	sprintf(reportBuf, "%s,%d,%d,%d,%d,%d,%d,%d", "SUPERBLOCK", num_blocks, num_inodes, 
 			block_size, inode_size, blocks_per_group, inodes_per_group, first_nonres_inode);
 	
+	if (sb.s_magic != EXT2_SUPER_MAGIC)
+	    exit(1); 		
+
 	printf("%s\n", reportBuf);
 }
 
 void groupSummary() {
+	pread(ext2_fd, &groupDesc, sizeof(groupDesc), 1024+block_size);
 	num_group = 0;
-	//blocks_per_group also included here
-	//inodes_per_group also included here
+	//num_blocks also included here
+	//num_inodes also included here
 	free_blocks = groupDesc.bg_free_blocks_count;
 	free_inodes = groupDesc.bg_free_inodes_count;	
 	block_bitmap_blk_num = groupDesc.bg_block_bitmap;
 	inode_bitmap_blk_num = groupDesc.bg_inode_bitmap;	
         first_inode_blk_num = groupDesc.bg_inode_table;	
 
-	pread(ext2_fd, &groupDesc, sizeof(groupDesc), 1024+block_size);
-	sprintf(reportBuf, "%s,%d,%d,%d,%d,%d,%d,%d,%d", "GROUP", num_group, blocks_per_group, 
-			inodes_per_group, free_blocks, free_inodes, block_bitmap_blk_num, inode_bitmap_blk_num,
+	sprintf(reportBuf, "%s,%d,%d,%d,%d,%d,%d,%d,%d", "GROUP", num_group, num_blocks, 
+			num_inodes, free_blocks, free_inodes, block_bitmap_blk_num, inode_bitmap_blk_num,
 			first_inode_blk_num);
-
-	if (sb.s_magic != EXT2_SUPER_MAGIC)
-	    exit(1); 	
 
 	printf("%s\n", reportBuf);
 }
@@ -77,7 +77,7 @@ int main(int argc, char** argv) {
 		exit(1);
 	}
 	
-	ext2_fd = open("EXT2_test.img", O_RDONLY);   
+	ext2_fd = open("EXT2_test.img", O_RDONLY);   	
 
 	//------------get summaries------------------------------
 	superblockSummary();
